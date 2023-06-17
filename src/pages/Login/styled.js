6e38bdb49adb53ecea9fd2b667927159bf3acfb0
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-06-16 23:31:28 -0400

    feat: login page styling

--- src/pages/Login/styled.js
+++ src/pages/Login/styled.js
@@ -1,13 +1,3 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  background: burlywood;
-
-  small {
-    font-size: 12pt;
-    margin-left: 15px;
-    color: #999;
-  }
-`;
-
 export const Paragrafo = styled.p``;
