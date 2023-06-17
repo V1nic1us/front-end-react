8c90286d4c2510b333bfb3b4d23e60413ea67d2f
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-06-16 23:34:23 -0400

    feat: passing the path of routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,34 +1,22 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
-import { toast } from 'react-toastify';
 import MyRoute from './MyRoute';
+import Aluno from '../pages/Aluno';
+import Alunos from '../pages/Alunos';
+import Fotos from '../pages/Fotos';
+import Register from '../pages/Register';
 import Login from '../pages/Login';
 import Page404 from '../pages/Page404';
 
 export default function Routes() {
-  toast.success('sucesso', {
-    toastId: 'sucesso',
-    position: 'top-right',
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    draggable: true,
-    progress: undefined,
-    theme: 'light',
-  });
-  toast.error('erro', {
-    toastId: 'erro',
-    position: 'top-right',
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    draggable: true,
-    progress: undefined,
-    theme: 'light',
-  });
   return (
     <Switch>
-      <MyRoute exact path="/" component={Login} />
+      <MyRoute exact path="/" component={Alunos} isClosed={false} />
+      <MyRoute exact path="/aluno/:id/edit" component={Aluno} isClosed />
+      <MyRoute exact path="/aluno" component={Aluno} isClosed />
+      <MyRoute exact path="/fotos/:id" component={Fotos} isClosed />
+      <MyRoute exact path="/login" component={Login} isClosed={false} />
+      <MyRoute exact path="/register" component={Register} isClosed={false} />
       <MyRoute path="*" component={Page404} />
     </Switch>
   );
