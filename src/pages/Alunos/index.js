46836b0b1a8ea8ec21b2cd067e8073fd7e06b905
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-06-20 23:10:59 -0400

    feat: querying the API and displaying the data

--- src/pages/Alunos/index.js
+++ src/pages/Alunos/index.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { get } from 'lodash';
+import { FaUserCircle } from 'react-icons/fa';
 import { Container } from '../../styles/GlobalStyles';
+import {
+  AlunoContainer,
+  ProfilePicture,
+  StyledFaEdit,
+  StyledFaWindowClose,
+} from './styled';
+import axios from '../../services/axios';
 
 export default function Alunos() {
+  const [alunos, setAlunos] = useState([]);
+  useEffect(() => {
+    async function getData() {
+      const response = await axios.get('/alunos');
+      setAlunos(response.data);
+    }
+
+    getData();
+  }, []);
+
   return (
     <Container>
-      <h1>Login</h1>
+      <h1>Alunos</h1>
+      <AlunoContainer>
+        {alunos.map((aluno) => (
+          <div key={String(aluno.id)}>
+            <ProfilePicture>
+              {get(aluno, 'Fotos[0].url', false) ? (
+                <img src={aluno.Fotos[0].url} alt="" />
+              ) : (
+                <FaUserCircle size={36} />
+              )}
+            </ProfilePicture>
+
+            <span>{aluno.nome}</span>
+            <span>{aluno.email}</span>
+
+            <Link to={`/aluno/${aluno.id}/edit`}>
+              <StyledFaEdit size={24} />
+            </Link>
+
+            <Link to={`/aluno/${aluno.id}/delete`}>
+              <StyledFaWindowClose size={24} />
+            </Link>
+          </div>
+        ))}
+      </AlunoContainer>
     </Container>
   );
 }
