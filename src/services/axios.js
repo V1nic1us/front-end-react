7bb31cc4f00910abb5581389a0fa08b00191e349
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-06-21 23:54:38 -0400

    feat: added login required in axios

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 const url = process.env.REACT_APP_API_URL;
+const token = process.env.REACT_APP_TOKEN;
 
 export default axios.create({
   baseURL: url,
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
 });
