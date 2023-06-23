5a52f047190349e71934ea35bec92af33ae43e78
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-06-23 14:24:17 -0400

    feat: persisting data in redux

--- src/store/modules/reduxPersist.js
+++ src/store/modules/reduxPersist.js
@@ -4,9 +4,9 @@ import { persistReducer } from 'redux-persist';
 export default (reducers) => {
   const persistedReducers = persistReducer(
     {
-      key: 'REACT-BASE',
+      key: 'front-end-react',
       storage,
-      whitelist: ['example'],
+      whitelist: ['auth'],
     },
     reducers
   );
