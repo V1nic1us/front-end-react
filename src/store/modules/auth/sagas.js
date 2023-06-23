7436f9b1eea338888a6dafe142596fec255f20d6
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-06-23 17:50:16 -0400

    feat: added auth saga

---
+++ src/store/modules/auth/sagas.js
@@ -0,0 +1,8 @@
+import { all, takeLatest } from 'redux-saga/effects';
+import * as types from '../types';
+
+function* loginRequest({ payload }) {
+  yield console.log('SAGA', payload);
+}
+
+export default all([takeLatest(types.LOGIN_REQUEST, loginRequest)]);
