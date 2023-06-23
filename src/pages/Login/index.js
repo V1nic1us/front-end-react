9a8921842c28abecce879de167398a9283903f68
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-06-23 14:22:55 -0400

    feat: creating the login screen and saving data

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import { isEmail } from 'validator';
+import { useDispatch } from 'react-redux';
 import { Container } from '../../styles/GlobalStyles';
+import { Form } from './styled';
+import * as actions from '../../store/modules/auth/actions';
 
 export default function Aluno() {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    let formErrors = false;
+
+    if (!isEmail(email)) {
+      formErrors = true;
+      toast.error('Email invalido');
+    }
+
+    if (password.length < 6 || password.length > 50) {
+      formErrors = true;
+      toast.error('Senha invalido');
+    }
+
+    if (formErrors) return;
+
+    dispatch(actions.loginRequest({ email, password }));
+  }
+
   return (
     <Container>
       <h1>Login</h1>
+      <Form onSubmit={(e) => handleSubmit(e)}>
+        <label htmlFor="email">
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Digite seu email"
+          />
+        </label>
+
+        <label htmlFor="password">
+          Senha:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Digite sua Senha"
+          />
+        </label>
+
+        <button type="submit">Acessar</button>
+      </Form>
     </Container>
   );
 }
