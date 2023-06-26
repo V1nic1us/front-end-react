05dfc5480b13001bd0022ece999abae2ed22e142
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-06-26 16:45:00 -0400

    feat: created loading component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Container } from './styled';
+
+export default function Loading({ isLoading }) {
+  if (!isLoading) return null;
+  return (
+    <Container>
+      <div />
+      <span>Carregando...</span>
+    </Container>
+  );
+}
+
+Loading.defaultProps = {
+  isLoading: false,
+};
+
+Loading.defaultProps = {
+  isLoading: PropTypes.bool,
+};
