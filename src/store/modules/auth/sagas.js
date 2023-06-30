2f10884f74cda02e012d6718f53ce65ca291be59
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-06-29 23:53:35 -0400

    feat: user creation and editing via axios in saga

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -6,6 +6,8 @@ import * as types from '../types';
 import axios from '../../../services/axios';
 import history from '../../../services/history';
 
+/* eslint-disable */
+
 function* loginRequest({ payload }) {
   try {
     const response = yield call(axios.post, '/tokens', payload);
@@ -25,7 +27,53 @@ function persistRehydrate({ payload }) {
   axios.defaults.headers.Authorization = `Bearer ${token}`;
 }
 
+function* registerRequest({ payload }) {
+  const { id, nome, email, password } = payload;
+
+  try {
+    if (id) {
+      yield call(axios.put, '/users', {
+        email,
+        nome,
+        password: password || undefined,
+      });
+      toast.success('Conta alterada com sucesso');
+      yield put(actions.registerUpdatedSuccess({ nome, email, password }));
+    } else {
+      yield call(axios.post, '/users', {
+        email,
+        nome,
+        password,
+      });
+      toast.success('Conta criada com sucesso');
+      yield put(actions.registerCreatedSuccess({ nome, email, password }));
+      console.log('entrou aqui');
+      return history.push('/login');
+    }
+  } catch (error) {
+    console.log('aqui');
+    const e = get(error, 'response.data.errors', []);
+
+    const status = get(error, 'response.status', 0);
+
+    if (status === 401) {
+      toast.error('Token expirado voce precisa logar novamente');
+      yield put(actions.loginFailure());
+      history.push('/login');
+    }
+
+    if (e.length > 0) {
+      e.map((errors) => toast.error(errors));
+    } else {
+      toast.error('Erro desconhecido');
+    }
+
+    yield put(actions.registerFailure());
+  }
+}
+
 export default all([
   takeLatest(types.LOGIN_REQUEST, loginRequest),
   takeLatest(types.PERSIST_REHYDRATE, persistRehydrate),
+  takeLatest(types.REGISTER_REQUEST, registerRequest),
 ]);
