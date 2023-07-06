1d97ffcb700d335a9b60f9c058d98652be8b4e75
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-07-05 21:32:17 -0400

    fix: removed unused token when using axios

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 const url = process.env.REACT_APP_API_URL;
-const token = process.env.REACT_APP_TOKEN;
 
 export default axios.create({
   baseURL: url,
-  headers: {
-    Authorization: `Bearer ${token}`,
-  },
 });
